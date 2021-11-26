ca561d7b3098c1c9132ea07c32c5d42f03079c85
trananhminh <trananhminh910@gmail.com>
2021-11-26 13:56:33 +0700

    Login and Logout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: "trananhminh910@gmail.com",
+  name: "Tran Anh Minh",
+  gender: true,
+  avatar: "1.png",
+  role: 1,
+  password: "111111",
+  password_confirmation: "111111",
+  is_active: true
+)
+30.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  gender = true
+  password = "password"
+  User.create!(
+    name: name,
+    email: email,
+    gender: gender,
+    avatar: "1.png",
+    role: 0,
+    password: password,
+    password_confirmation: password,
+    is_active: true,
+  )
+end
