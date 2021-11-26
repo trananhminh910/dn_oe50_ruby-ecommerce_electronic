09590929261e2e33e40addaab435d36cac6f518f
luandang4 <luandang4@gmail.com>
2021-11-26 16:52:37 +0700

    Show product for user screen

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,4 +1,6 @@
 class Rate < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  scope :rate_product, ->(product_id){where product_id: product_id}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_page#home"
     get "/cart", to: "static_page#cart"
-    get "/detail", to: "static_page#detail"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    resources :products, only: [:show]
     namespace :admin do
       root "static_pages#index"
     end
