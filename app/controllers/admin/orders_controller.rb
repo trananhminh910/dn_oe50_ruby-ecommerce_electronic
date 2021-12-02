fb1769f60d5dab267137e1b9930dfad68cf9d868
trananhminh <trananhminh910@gmail.com>
2021-12-02 17:26:18 +0700

    Show orders admin and search, filter, paginate

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,3 +1,31 @@
 class Admin::OrdersController < Admin::BaseController
-  def index; end
+  def index
+    if search_params?
+      search_with_sort params[:search], params[:option]
+      @orders = @orders.page(params[:page])
+                       .per(Settings.size.page_record_medium)
+    else
+      @orders = Order.all
+                     .page(params[:page]).per(Settings.size.page_record_medium)
+    end
+  end
+
+  private
+
+  def search_with_sort search_keyword, option
+    @orders = case option
+              when Settings.admin.order_option.oldest
+                Order.search(search_keyword).order_oldest
+              when Settings.admin.order_option.newest
+                Order.search(search_keyword).order_newest
+              when Settings.admin.order_option.status
+                Order.search(search_keyword).order_status
+              else
+                Order.search search_keyword
+              end
+  end
+
+  def search_params?
+    params[:search] && params[:option]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,72 @@ User.create!(
     is_active: true,
   )
 end
+
+5.times do |n|
+  name = %w[laptop PC tablet mobile headphone].sample
+  parent_id = 1
+  Category.create!(
+    name: name,
+    parent_id: parent_id
+  )
+end
+
+30.times do |n|
+  name = User.find(rand(1..20)).name
+  address = Faker::Address.city
+  phone = Faker::PhoneNumber.cell_phone
+  user_id = rand(1..20)
+  Address.create!(
+    name: name,
+    address: address,
+    phone: phone,
+    user_id: user_id
+  )
+end
+
+30.times do |n|
+  name = Faker::Commerce.product_name
+  price = 200000
+  image = "1.png"
+  discount = 20
+  residual = 100
+  description = Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4)
+  category_id = 1
+  Product.create!(
+    name: name,
+    price: price,
+    image: image,
+    discount: discount,
+    residual: residual,
+    description: description,
+    category_id: category_id
+  )
+end
+
+30.times do |n|
+  status = rand(0..3)
+  shipping_fee = 20000
+  total_of_money = 222222
+  user_id = rand(1..20)
+  address_id = rand(1..20)
+  Order.create!(
+    status: status,
+    shipping_fee: shipping_fee,
+    total_of_money: total_of_money,
+    user_id: user_id,
+    address_id: address_id
+  )
+end
+
+30.times do |n|
+  quantity = %w[0 1 2 3]
+  autual_price = 200000
+  order_id = rand(1..20)
+  product_id = rand(1..20)
+  OrderDetail.create!(
+    quantity: quantity,
+    autual_price: autual_price,
+    order_id: order_id,
+    product_id: product_id
+  )
+end
