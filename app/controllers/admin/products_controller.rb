580491d6253ff6636dc8b30089aac6bac2cf4af1
trananhminh <trananhminh910@gmail.com>
2021-12-03 22:58:55 +0700

    Show list products admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,3 +1,32 @@
 class Admin::ProductsController < Admin::BaseController
-  def index; end
+  def index
+    if search_params?
+      search_with_sort params[:search], params[:option]
+      @products = @products.page(params[:page])
+                           .per(Settings.size.page_record_medium)
+    else
+      @products = Product.all
+                         .page(params[:page])
+                         .per(Settings.size.page_record_medium)
+    end
+  end
+
+  private
+
+  def search_params?
+    params[:search] && params[:option]
+  end
+
+  def search_with_sort search_keyword, option
+    @products = case option
+                when Settings.admin.product_option.oldest
+                  Product.search(search_keyword).products_oldest
+                when Settings.admin.product_option.newest
+                  Product.search(search_keyword).products_newest
+                when Settings.admin.product_option.price
+                  Product.search(search_keyword).asc_order
+                else
+                  Product.search search_keyword
+                end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,12 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :order_details, dependent: :destroy
   has_many :rates, dependent: :destroy
+  enum sort: {oldest: 0, newest: 1, price: 2, default: 3}
 
   scope :asc_order, ->{order :price}
+  scope :products_oldest, ->{order :updated_at}
+  scope :products_newest, ->{order updated_at: :desc}
+  scope :search, ->(keywrd){where("products.name LIKE ? or products.description LIKE ?", "%#{keywrd}%", "%#{keywrd}%")}
 
   def check_residual_quantity? quantity_params
     quantity_params.positive? && residual >= quantity_params
